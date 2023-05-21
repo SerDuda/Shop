930f35d895c7a1ccb4518109489122ef28864534
Serhii <dudarchukserhii03@gmail.com>
2023-05-21 17:06:43 +0300

    add outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import './App.scss';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage'
 import ProductPage from './pages/ProductPage/ProductPage'
@@ -8,22 +6,23 @@ import CartPage from './pages/CartPage/CartPage';
 import Laptops from './pages/Category/Laptops/Laptops';
 import Smartphones from './pages/Category/Smartphones/Smartphones';
 import AllProducts from './pages/Category/AllProducts/AllProducts'
+import MainLayout from './components/layouts/Mainlayout';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header/>
-          <Routes>
-            <Route path='/' element={<HomePage/>}/>
-            <Route path='/product/:id' element={<ProductPage/>}/>
-            <Route path='/cart' element={<CartPage/>}/>
-            <Route path='/category/laptops' element={<Laptops/>}/>
-            <Route path='/category/smartphones' element={<Smartphones/>}/>
-            <Route path='/category/allproducts' element={<AllProducts/>}/>
+        <Routes>
+          <Route path='/' element={<MainLayout/>}>
+            <Route path='' element={<HomePage />} />
+            <Route path='/product/:id' element={<ProductPage />} />
+            <Route path='/cart' element={<CartPage />} />
+            <Route path='/category/laptops' element={<Laptops />} />
+            <Route path='/category/smartphones' element={<Smartphones />} />
+            <Route path='/category/allproducts' element={<AllProducts />} />
             <Route />
-          </Routes>
-        <Footer/>
+          </Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
